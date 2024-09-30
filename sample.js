72b536a3ff9bb3d058b3100d1cbe43a9d56ee662
M3hTi <mehdinasir502@yahoo.com>
2024-09-30 16:24:33 +0330

    add  second method for showing list

--- sample.js
+++ sample.js
@@ -1,10 +1,13 @@
 const inputElement = document.getElementById('item-input')
 const ulElement = document.getElementById('item-list')
 const btnElement = document.getElementById('submit-btn')
+// console.dir(btnElement)
 
 let lists = []
 
 
+//NOTE: This is a function to show the list(first method)
+/*
 const showList = function () {
     const value = inputElement.value
     // console.log(value);
@@ -18,6 +21,31 @@ const showList = function () {
             li.innerText = list
             ulElement.appendChild(li)
         });
+        inputElement.value = ''
+    }
+}
+*/
+
+
+//NOTE: This is a function to show the list(second method)
+const showList = function () {
+    const value = inputElement.value
+    // console.log(value);
+    if (value !== '') {
+        lists.push(value)
+        // console.log(lists);
+        // Clear the existing list
+        let html = ''
+        for (let index = 0; index < lists.length; index++) {
+            const list = lists[index];
+            let htmlElement  = `
+                <li>
+                    <span>${list}</span>
+                </li>
+            `
+            html += htmlElement
+        }
+        ulElement.innerHTML = html
         inputElement.value = ''
     }
 }
